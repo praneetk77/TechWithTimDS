0113c517f22368f951b562b10e7eb00c5500d7fd
praneetk77 <praneetkarna77@gmail.com>
2022-08-08 03:38:14 +0530

    Added plotting and saving best models functionality

--- LinearRegression.py
+++ LinearRegression.py
@@ -3,6 +3,9 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import pickle
+import matplotlib.pyplot as pyplot
+from matplotlib import style
 
 data = pd.read_csv('student-mat.csv', sep=';')
 data = data[['G1','G2','G3','studytime','failures','absences']]
@@ -10,13 +13,27 @@ predict = 'G3'
 
 x = np.array(data.drop([predict],1)) #attributes
 y = np.array(data[predict]) #labels
-
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x,y,test_size=0.1)
+best = 0
 
-linear = linear_model.LinearRegression()
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print(acc)
+'''for _ in range(100):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x,y,test_size=0.1)
+
+    #skipping the training process and loading the model from the pickle file
+    linear = linear_model.LinearRegression()
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print(acc)
+
+    if acc>best:
+        best = acc
+        with open("studentmodel.pickle","wb") as f:
+            pickle.dump(linear,f)'''
+
+print("Best : ", best)
+
+pickle_in = open("studentmodel.pickle","rb")
+linear = pickle.load(pickle_in)
 
 print("Constants : ", linear.coef_)
 print("Intercept : ", linear.intercept_)
@@ -25,3 +42,10 @@ predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+style.use("ggplot")
+p = "absences"
+pyplot.scatter(data[p],data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final grade")
+pyplot.show()
